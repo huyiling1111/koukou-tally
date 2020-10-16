9f05966961143b6236a358e0bfca24e6aabda9b8
huyiling1111 <1052859577@qq.com>
2020-10-17 00:10:31 +0800

    import一个svg目录

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg><use xlink:href="#labels" /></svg>
+      记账</router-link
+    >
     <router-link to="/labels">标签</router-link>
     <router-link to="/statistics">统计 </router-link>
   </div>
 </template>
 
 <script lang="ts">
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
 export default {
   name: "Nav",
 };
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons') // 当前目录
+
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)
+      .include.add(dir).end() // 只包含icons目录
+      .use('svg-sprite-loader').loader('svg-sprite-loader')
+      .options({ extract: false }).end() //不需要解析成文件
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{ plainSprite: true }])
+    config.module.rule('svg').exclude.add(dir) // 其他svg loader排除icons目录
+
+
+  }
+
 }
