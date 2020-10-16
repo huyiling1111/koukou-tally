ae16c4b01c63409da413669c631d9727b4a5b57e
huyiling1111 <1052859577@qq.com>
2020-10-16 22:09:10 +0800

    money布局

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,9 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Money
+  <div class="nav-wrapper">
+    <div class="content">Money</div>
+
     <Nav />
   </div>
 </template>
@@ -10,4 +11,14 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-</style>>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  flex-grow: 1;
+  overflow: auto;
+}
+</style>
